895c24c10e38c3599db896385cd055ed3daf70fe
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-07-17 22:00:44 +0530

    Intialization of the slices using makefunction

--- 10_slices/main.go
+++ 10_slices/main.go
@@ -5,6 +5,7 @@ import "fmt"
 // a slice is a flexible and dynamic data structure that provides a more powerful alternative to arrays
 // slice have a dynamic size and their length can be changed during the program's execution
 
+// use of make function : you can use the make function to create a slice with a specific length and capacity.
 func main() {
 	fmt.Println("Slices in golang")
 	// Declare and initialize a slice
@@ -18,6 +19,9 @@ func main() {
 
 	fmt.Println("Length of the Slice", len(numbers))
 
+	// Capacity of the Slice
+	fmt.Println("capacity of the Slice", cap(numbers))
+
 	// append in slice
 
 	numbers = append(numbers, 3, 10, 12, 34)
@@ -26,4 +30,19 @@ func main() {
 	// Intiatilization of empty slice
 	name := []string{}
 	fmt.Println("name:", name)
+
+	// Intialization of the slice using make function
+
+	numberwithmake := make([]int, 3, 5)
+
+	numberwithmake = append(numberwithmake, 10)
+	numberwithmake = append(numberwithmake, 98)
+
+	// If the capacity of the slice is full then it automatically double down its capacity
+
+	fmt.Println(" Slice initization uising make function")
+	fmt.Println("Slice", numberwithmake)
+	fmt.Println("Length of the Slice", len(numberwithmake))
+	fmt.Println("Capacity of the Slice", cap(numberwithmake))
+
 }
