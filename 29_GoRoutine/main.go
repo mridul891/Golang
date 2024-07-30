0a40837b4bb830c630a4f2f8ac5b3e7569d16308
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-07-30 19:59:08 +0530

    Goroutines in goLang

---
+++ 29_GoRoutine/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go Routine in Golang")
+	//	Gor ROutine are a key feature of the go programming language that allow you to run function concurrently  or in parallel , with other parts of your Program
+
+}
